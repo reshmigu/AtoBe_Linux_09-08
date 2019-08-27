efe2ea93d6d09632d80d100eacc588cf379b8b92
Nasia1234 <53426905+Nasia1234@users.noreply.github.com>
2019-08-27 16:15:40 +0530

    Update mail.java

--- src/main/java/com/test/mail.java
+++ src/main/java/com/test/mail.java
@@ -109,7 +109,7 @@ public class mail {
 			multipart.addBodyPart(messageBodyPart);
 			
 			messageBodyPart = new MimeBodyPart();
-			DataSource fds = new FileDataSource("/var/lib/jenkins/jobs/AtoBe_Linux_09-08/templates/images/ato-bee-logo.jpg");
+			DataSource fds = new FileDataSource(fullPath);
 			messageBodyPart.setDataHandler(new DataHandler(fds));
 			messageBodyPart.setHeader("Content-ID", "<logo>");
 			
@@ -119,7 +119,7 @@ public class mail {
 
 			multipart.addBodyPart(messageBodyPart);
 			messageBodyPart = new MimeBodyPart();
-			fds = new FileDataSource("/var/lib/jenkins/jobs/AtoBe_Linux_09-08/templates/images/banner.jpg");
+			fds = new FileDataSource(fullPath1);
 			messageBodyPart.setDataHandler(new DataHandler(fds));
 			messageBodyPart.setHeader("Content-ID", "<banner>");
 			
